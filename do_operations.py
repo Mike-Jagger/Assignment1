9a42ad499611800ad8c6ede6d46720e55cbb76f9
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2023-10-01 21:09:33 -0600

    Declaring functions in each project file

---
+++ do_operations.py
@@ -0,0 +1,7 @@
+#def my_mul()
+#def my_div()
+#def my_mul()
+#def my_div()
+
+#order_operations()
+#find_operation()
---
+++ user_input_lists.py
@@ -0,0 +1,4 @@
+#def validate_int_input()
+#def validate_operator_input()
+
+#generate_lists()
